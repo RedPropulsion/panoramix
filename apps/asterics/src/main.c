2b6291c8479a4b62bfce6933e9abfd8cc80bd724
TheRealLorenz <lollobellina@gmail.com>
2026-01-05 15:42:46 +0100

    feat: add simple blink + log

--- apps/asterics/src/main.c
+++ apps/asterics/src/main.c
@@ -1,9 +1,21 @@
+#include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
 
+const struct gpio_dt_spec spec = GPIO_DT_SPEC_GET(DT_NODELABEL(my_led), gpios);
+
 int main(void) {
+  if (!gpio_is_ready_dt(&spec)) {
+    return 1;
+  }
+
+  gpio_pin_configure_dt(&spec, GPIO_OUTPUT_HIGH);
+
+  int x = 0;
+
   while (1) {
-    printk("HEY!");
+    printk("HEY %d!\n", x++);
+    gpio_pin_toggle_dt(&spec);
     k_sleep(K_MSEC(1000));
   }
 }
