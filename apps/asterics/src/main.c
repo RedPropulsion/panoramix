7283b5b4d1f8f93d36195d63d298fde3c193ed7e
TheRealLorenz <lollobellina@gmail.com>
2026-01-06 12:37:07 +0100

    feat: blink leds + panic handler

--- apps/asterics/src/main.c
+++ apps/asterics/src/main.c
@@ -1,21 +1,62 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
+#include <zephyr/logging/log_ctrl.h>
 #include <zephyr/sys/printk.h>
 
-const struct gpio_dt_spec spec = GPIO_DT_SPEC_GET(DT_NODELABEL(my_led), gpios);
+#include <stddef.h>
+
+const struct gpio_dt_spec leds[] = {
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_1), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_2), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_3), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_4), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_5), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_6), gpios),
+    GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_7), gpios),
+};
 
 int main(void) {
-  if (!gpio_is_ready_dt(&spec)) {
-    return 1;
+  /* if (!gpio_is_ready_dt(&spec)) { */
+  /*   return 1; */
+  /* } */
+
+  for (size_t i = 0; i < (sizeof leds) / (sizeof leds[0]); i++) {
+    gpio_pin_configure_dt(&leds[i], GPIO_OUTPUT_LOW);
   }
-
-  gpio_pin_configure_dt(&spec, GPIO_OUTPUT_HIGH);
-
-  int x = 0;
 
   while (1) {
-    printk("HEY %d!\n", x++);
-    gpio_pin_toggle_dt(&spec);
+    for (size_t i = 0; i < (sizeof leds) / (sizeof leds[0]); i++) {
+      printk("Setting led %d!\n", i + 1);
+      gpio_pin_set_dt(&leds[i], 1);
+
+      k_sleep(K_MSEC(1000));
+    }
+
     k_sleep(K_MSEC(1000));
+
+    for (size_t i = 0; i < (sizeof leds) / (sizeof leds[0]); i++) {
+      printk("Resetting led %d!\n", i + 1);
+      gpio_pin_set_dt(&leds[i], 0);
+
+      k_sleep(K_MSEC(1000));
+    }
+
+    k_panic();
   }
+}
+
+void k_sys_fatal_error_handler(unsigned int reason,
+                               const struct arch_esf *esf) {
+  const struct gpio_dt_spec error_led =
+      GPIO_DT_SPEC_GET(DT_NODELABEL(led_mcu_1), gpios);
+
+  LOG_PANIC();
+
+  while (1) {
+    printk("I'M PANICKING\n");
+    gpio_pin_toggle_dt(&error_led);
+    k_busy_wait(500 * 1000);
+  }
+
+  k_fatal_halt(reason);
 }
