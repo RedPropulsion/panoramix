9dd4a75dc9b1e842bd2f228bb2c925ee0744b3cc
Matviy Starodubtsev <matviy.starodubtsev@edu.unifi.it>
2026-04-29 17:20:23 +0200

    Fix: moved UDP client to separate library.

--- modules/udp_client/udp_client.c
+++ modules/udp_client/udp_client.c
@@ -6,17 +6,40 @@ LOG_MODULE_REGISTER(udp_client, LOG_LEVEL_DBG);
 #include <zephyr/net/net_if.h>
 #include <zephyr/net/net_mgmt.h>
 #include <zephyr/net/net_event.h>
-#include <zephyr/posix/arpa/inet.h>
+#include <zephyr/net/net_ip.h>
 #include <zephyr/posix/poll.h>
 
 #include <string.h>
 
+#ifdef CONFIG_UDP_CLIENT_SERVER_IP
+#define UDP_SERVER_IP CONFIG_UDP_CLIENT_SERVER_IP
+#else
 #define UDP_SERVER_IP "192.168.1.1"
-#define UDP_SERVER_PORT 14550
-#define UDP_LOCAL_PORT 14551
+#endif
 
+#ifdef CONFIG_UDP_CLIENT_SERVER_PORT
+#define UDP_SERVER_PORT CONFIG_UDP_CLIENT_SERVER_PORT
+#else
+#define UDP_SERVER_PORT 14550
+#endif
+
+#ifdef CONFIG_UDP_CLIENT_LOCAL_PORT
+#define UDP_LOCAL_PORT CONFIG_UDP_CLIENT_LOCAL_PORT
+#else
+#define UDP_LOCAL_PORT 14551
+#endif
+
+#ifdef CONFIG_UDP_CLIENT_STACK_SIZE
+#define ETH_STACK_SIZE CONFIG_UDP_CLIENT_STACK_SIZE
+#else
 #define ETH_STACK_SIZE 2048
+#endif
+
+#ifdef CONFIG_UDP_CLIENT_PRIORITY
+#define ETH_PRIORITY CONFIG_UDP_CLIENT_PRIORITY
+#else
 #define ETH_PRIORITY 5
+#endif
 
 static K_SEM_DEFINE(net_ready, 0, 1);
 static struct k_thread eth_thread_data;
@@ -24,17 +47,11 @@ static K_KERNEL_STACK_DEFINE(eth_stack, ETH_STACK_SIZE);
 
 static int sock = -1;
 
-
-
-/*
- NOTE: Zephyr has PING support so you can use that to test connection without wireshark or server setup.
-	   Just ping the target IP (UDP_SERVER_IP) from your host machine and you should see the logs in the console.
-	   You can also send UDP packets to the same IP using netcat or similar tools to see the recv logs.
- */
-
 static void net_mgmt_cb(struct net_mgmt_event_callback *cb,
 			 uint64_t mgmt_event, struct net_if *iface)
 {
+	ARG_UNUSED(cb);
+	ARG_UNUSED(iface);
 	if (mgmt_event == NET_EVENT_IF_UP) {
 		k_sem_give(&net_ready);
 	}
@@ -59,37 +76,49 @@ static int wait_for_net(void)
 
 int udp_client_send(const void *data, size_t len)
 {
-	struct sockaddr_in server_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(UDP_SERVER_PORT),
-	};
+	struct net_sockaddr_in server_addr;
+	int ret;
 
-	inet_pton(AF_INET, UDP_SERVER_IP, &server_addr.sin_addr);
+	if (sock < 0) {
+		return -ENOTCONN;
+	}
 
-	return sendto(sock, data, len, 0,
-		      (struct sockaddr *)&server_addr, sizeof(server_addr));
+	server_addr.sin_family = NET_AF_INET;
+	server_addr.sin_port = net_htons(UDP_SERVER_PORT);
+	ret = net_addr_pton(NET_AF_INET, UDP_SERVER_IP, &server_addr.sin_addr);
+	if (ret < 0) {
+		return ret;
+	}
+
+	return zsock_sendto(sock, data, len, 0,
+			  (struct net_sockaddr *)&server_addr, sizeof(server_addr));
 }
 
 int udp_client_recv(void *buf, size_t max_len, int timeout_ms)
 {
-	struct sockaddr_in from;
-	socklen_t from_len = sizeof(from);
+	struct net_sockaddr_in from;
+	net_socklen_t from_len = sizeof(from);
 	struct pollfd pfd = {
 		.fd = sock,
 		.events = POLLIN,
 	};
 
+	if (sock < 0) {
+		return -ENOTCONN;
+	}
+
 	if (poll(&pfd, 1, timeout_ms) <= 0) {
 		return -EAGAIN;
 	}
 
-	return recvfrom(sock, buf, max_len, 0,
-			(struct sockaddr *)&from, &from_len);
+	return zsock_recvfrom(sock, buf, max_len, 0,
+			   (struct net_sockaddr *)&from, &from_len);
 }
 
 static void eth_thread(void *p1, void *p2, void *p3)
 {
 	int ret;
+	struct net_sockaddr_in local_addr;
 
 	ARG_UNUSED(p1);
 	ARG_UNUSED(p2);
@@ -99,22 +128,22 @@ static void eth_thread(void *p1, void *p2, void *p3)
 
 	wait_for_net();
 
-	sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+	sock = zsock_socket(NET_AF_INET, NET_SOCK_DGRAM, NET_IPPROTO_UDP);
 	if (sock < 0) {
 		LOG_ERR("Failed to create socket: %d", -errno);
 		return;
 	}
 
-	struct sockaddr_in local_addr = {
-		.sin_family = AF_INET,
-		.sin_port = htons(UDP_LOCAL_PORT),
-		.sin_addr.s_addr = htonl(INADDR_ANY),
-	};
+	memset(&local_addr, 0, sizeof(local_addr));
+	local_addr.sin_family = NET_AF_INET;
+	local_addr.sin_port = net_htons(UDP_LOCAL_PORT);
+	local_addr.sin_addr.s_addr = net_htonl(NET_ADDR_ANY);
 
-	ret = bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr));
+	ret = zsock_bind(sock, (struct net_sockaddr *)&local_addr, sizeof(local_addr));
 	if (ret < 0) {
 		LOG_ERR("Failed to bind socket: %d", -errno);
 		zsock_close(sock);
+		sock = -1;
 		return;
 	}
 
@@ -141,13 +170,14 @@ static void eth_thread(void *p1, void *p2, void *p3)
 	}
 }
 
-void udp_client_init(void)
+int udp_client_init(void)
 {
 	LOG_INF("Initializing UDP client --- Make sure Ethernet cable is connected!");
 	k_thread_create(&eth_thread_data, eth_stack,
 			K_KERNEL_STACK_SIZEOF(eth_stack),
 			eth_thread, NULL, NULL, NULL,
 			ETH_PRIORITY, 0, K_NO_WAIT);
+	return 0;
 }
 
 void udp_client_deinit(void)
