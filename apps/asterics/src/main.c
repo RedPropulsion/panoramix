091e5d0d7dc44fa96dc5533f87374597a8a9ee35
TheRealLorenz <lollobellina@gmail.com>
2026-05-19 18:52:34 +0200

    feat(smf): manual mode to wiggle servos

--- apps/asterics/src/main.c
+++ apps/asterics/src/main.c
@@ -1,4 +1,5 @@
-#include <string.h>
+#include <stddef.h>
+
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
@@ -6,9 +7,19 @@
 #include <zephyr/logging/log_ctrl.h>
 #include <zephyr/smf.h>
 
-#include <stddef.h>
+#include <mavwrap.h>
 
 LOG_MODULE_REGISTER(main);
+
+#define RX_QUEUE_SIZE 16
+
+K_MSGQ_DEFINE(rx_queue, sizeof(mavlink_message_t), RX_QUEUE_SIZE,
+              sizeof(void *));
+
+const struct device *servos[] = {
+    DEVICE_DT_GET(DT_NODELABEL(servo_main_pwm)),
+    DEVICE_DT_GET(DT_NODELABEL(servo_drogue_pwm)),
+};
 
 struct exec_ctx {
   // MUST be the first element in the struct
@@ -19,15 +30,80 @@ enum state { BOOT, IDLE, CALIBRATION, MANUAL, STREAM, ARMED, LAUNCH };
 // Forward declaration
 const struct smf_state states[];
 
+static void set_servo(uint8_t servo_id, uint32_t deg) {
+  if (servo_id >= ARRAY_SIZE(servos)) {
+    LOG_ERR("Invalid servo_id: %d", servo_id);
+    return;
+  }
+}
+
 static enum smf_state_result boot_run(void *o) {
-  LOG_ERR("Not implemented");
+  LOG_WRN("Not implemented");
 
   smf_set_state(SMF_CTX(&exec_ctx_obj), &states[IDLE]);
   return SMF_EVENT_HANDLED;
 }
 
 static enum smf_state_result idle_run(void *o) {
-  LOG_ERR("Not implemented");
+  mavlink_message_t message;
+  int ret = k_msgq_get(&rx_queue, &message, K_NO_WAIT);
+  if (ret < 0) {
+    return SMF_EVENT_HANDLED;
+  }
+
+  LOG_INF("Got message id: %d", message.msgid);
+
+  if (message.msgid == MAVLINK_MSG_ID_COMMAND_LONG) {
+    mavlink_command_long_t cmd;
+    mavlink_msg_command_long_decode(&message, &cmd);
+
+    if (cmd.command == MAV_CMD_DO_SET_MODE) {
+      switch ((int)cmd.param2) {
+      case 12:
+        LOG_INF("GOING TO MANUAL MODE");
+        smf_set_state(SMF_CTX(&exec_ctx_obj), &states[MANUAL]);
+        break;
+      default:
+        LOG_ERR("Invalid custom mode code %d", (int)cmd.param2);
+        break;
+      }
+    } else {
+      LOG_ERR("Invalid cmd %d", cmd.command);
+    }
+  }
+
+  return SMF_EVENT_HANDLED;
+}
+
+static enum smf_state_result manual_run(void *o) {
+  mavlink_message_t message;
+  int ret = k_msgq_get(&rx_queue, &message, K_NO_WAIT);
+  if (ret < 0) {
+    return SMF_EVENT_HANDLED;
+  }
+
+  LOG_INF("Got message id: %d", message.msgid);
+
+  if (message.msgid == MAVLINK_MSG_ID_COMMAND_LONG) {
+    mavlink_command_long_t cmd;
+    mavlink_msg_command_long_decode(&message, &cmd);
+
+    if (cmd.command == MAV_CMD_DO_SET_MODE) {
+      switch ((int)cmd.param2) {
+      case 14:
+        LOG_INF("GOING TO IDLE MODE");
+        smf_set_state(SMF_CTX(&exec_ctx_obj), &states[MANUAL]);
+        break;
+      default:
+        LOG_ERR("Invalid custom mode code %d", (int)cmd.param2);
+        break;
+      }
+    } else if (cmd.command == MAV_CMD_DO_SET_SERVO) {
+      set_servo(cmd.param1, cmd.param2);
+    } else {
+      LOG_ERR("Invalid cmd %d", cmd.command);
+    }
+  }
 
   return SMF_EVENT_HANDLED;
 }
@@ -36,7 +112,7 @@ const struct smf_state states[] = {
     [BOOT] = SMF_CREATE_STATE(NULL, boot_run, NULL, NULL, NULL),
     [IDLE] = SMF_CREATE_STATE(NULL, idle_run, NULL, NULL, NULL),
     [CALIBRATION] = SMF_CREATE_STATE(NULL, NULL, NULL, NULL, NULL),
-    [MANUAL] = SMF_CREATE_STATE(NULL, NULL, NULL, NULL, NULL),
+    [MANUAL] = SMF_CREATE_STATE(NULL, manual_run, NULL, NULL, NULL),
     [STREAM] = SMF_CREATE_STATE(NULL, NULL, NULL, NULL, NULL),
     [ARMED] = SMF_CREATE_STATE(NULL, NULL, NULL, NULL, NULL),
     [LAUNCH] = SMF_CREATE_STATE(NULL, NULL, NULL, NULL, NULL),
